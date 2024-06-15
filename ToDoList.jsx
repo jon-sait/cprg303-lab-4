884b5f6c6285ce160bc7140a1558f1205e2b1b66
Jonathan <jincheng.lei@edu.sait.ca>
2024-06-15 13:35:41 +0800

    Refactor components

---
+++ ToDoList.jsx
@@ -0,0 +1,27 @@
+import { Pressable, View, Text, ScrollView } from "react-native";
+
+import { styles } from "./App";
+
+function ToDoList() {
+  return (
+    <ScrollView>
+      <Pressable>
+        <View style={[styles.task, styles.completed]}>
+          <Text style={styles.taskText}>Do laundry</Text>
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={[styles.task]}>
+          <Text style={styles.taskText}>Go to gym</Text>
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={[styles.task, styles.completed]}>
+          <Text style={styles.taskText}>Walk dog</Text>
+        </View>
+      </Pressable>
+    </ScrollView>
+  );
+}
+
+export default ToDoList;
