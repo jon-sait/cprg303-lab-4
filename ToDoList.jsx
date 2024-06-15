364b54a270c10429c3ea2759d0d5a067d0632371
Jonathan <jincheng.lei@edu.sait.ca>
2024-06-15 13:45:48 +0800

    Props and state management

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -2,24 +2,18 @@ import { Pressable, View, Text, ScrollView } from "react-native";
 
 import { styles } from "./App";
 
-function ToDoList() {
+function ToDoList({ tasks }) {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {
+        tasks.map(task =>
+          <Pressable key={task}>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>{task}</Text>
+            </View>
+          </Pressable>
+        )
+      }
     </ScrollView>
   );
 }
